ba460c6641f3c653b6996340f2557e6f001d055a
Sam Hakkak <samhakkak@Sams-MacBook-Pro-2.local>
2022-09-15 12:43:09 +0100

    lots of updates, midway through chapter 4

---
+++ prod/services/webserver-cluster/main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "us-east-2"
+}
+
+module "webserver_cluster" {
+    source = "../../../modules/services/webserver-cluster"
+
+    cluster_name            = "webservers-prod"
+    db_remote_state_bucket  = "fridgemagnet-terraform-state"
+    db_remote_state_key     = "prod/data-stores/mysql/terraform.tfstate"
+}
